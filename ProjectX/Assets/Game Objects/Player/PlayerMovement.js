9e03d9ca5001b5b36420ea57c41ad9f77f3de270
Uriel Carrillo <carrillouriel@gmail.com>
2016-03-06 20:38:17 -0700

    Base project, character movement

---
+++ ProjectX/Assets/Game Objects/Player/PlayerMovement.js
@@ -0,0 +1,30 @@
+﻿var speed : float = 1; 
+
+
+
+//Will check every frame
+function Update () {
+
+
+//Go Left
+if (Input.GetKey ("left")) 
+{
+transform.Translate(Vector3(-speed,0,0) * Time.deltaTime);
+ //GetComponent.<Rigidbody2D>().AddForce(Vector2(-1,0) * 10);
+}
+
+//Go right
+if (Input.GetKey ("right"))
+{
+transform.Translate(Vector3(speed,0,0) * Time.deltaTime);
+ //GetComponent.<Rigidbody2D>().AddForce(Vector2.right * 10);
+}
+
+//Jump
+if(Input.GetKeyDown(KeyCode.Space))
+{
+ GetComponent.<Rigidbody2D>().AddForce(new Vector2(0, 300 ));
+}
+
+
+}
