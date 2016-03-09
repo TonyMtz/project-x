3cad02703dba05ac6f922219b599c968794d10d7
Uriel Carrillo <carrillouriel@gmail.com>
2016-03-08 23:05:22 -0700

    Display current enemies HP and Max HP above head, ugly as hell, but it's only for debugging

---
+++ ProjectX/Assets/Scripts/EnemyUIController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyUIController : MonoBehaviour {
+
+	public TextMesh EnemyHP;
+	public Character Enemy;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string enemyCurrentHP=Enemy.getCurrentHealthPoints ().ToString ();
+		string enemyMaxHP = Enemy.getMaxHealthPoints ().ToString ();
+		EnemyHP.text = "HP: " + enemyCurrentHP+"/"+enemyMaxHP;
+	}
+}
