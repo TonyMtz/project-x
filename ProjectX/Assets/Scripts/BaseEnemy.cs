539c602f7574bdef498a1abc08c601352ca931bc
tonymtz <atony.mtz@gmail.com>
2016-03-08 23:53:04 -0700

    New enemies abstraction. Dummy enemy added

---
+++ ProjectX/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class BaseEnemy : Character
+{
+    [SerializeField]
+    protected Transform[] collisionCheck;
+
+    [SerializeField]
+    protected float collisionCheckRadius;
+
+    [SerializeField]
+    protected LayerMask whatIsCollision;
+
+    // Use this for initialization
+    override public void Start()
+    {
+        base.Start();
+        isFacingLeft = true;
+    }
+}
---
+++ ProjectX/Assets/Scripts/Enemies/Dummy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Dummy : BaseEnemy
+{
+    // FixedUpdate is called every fixed framerate frame
+    void FixedUpdate()
+    {
+        if (IsWalled())
+        {
+            isFacingLeft = !isFacingLeft;
+            Flip();
+        }
+
+        Move();
+    }
+
+    // Check if entity collisions with a wall/ground
+    private bool IsWalled()
+    {
+        bool isWalled = false;
+
+        foreach (Transform wall in collisionCheck)
+        {
+            isWalled = Physics2D.OverlapCircle(wall.position, collisionCheckRadius, whatIsCollision);
+            if (isWalled)
+            {
+                break;
+            }
+        }
+
+        return isWalled;
+    }
+}
