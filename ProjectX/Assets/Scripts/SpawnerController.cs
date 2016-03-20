5974b2bde7d57be3f4aeec6d4016c16ae074ba1e
Uriel Carrillo <carrillouriel@gmail.com>
2016-03-20 10:38:57 -0700

    add mob spawner and promo images

---
+++ ProjectX/Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerController : MonoBehaviour {
+
+	public GameObject dummy;
+
+	// Use this for initialization
+	void Start () {
+		SpawnMobsEverySecond();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnMobsEverySecond(){
+		InvokeRepeating("SpawnMobs", 2, 1F);
+	}
+
+	void SpawnMobs() {
+		GameObject wreckClone = (GameObject) Instantiate(dummy, transform.position, transform.rotation);
+	}
+}
